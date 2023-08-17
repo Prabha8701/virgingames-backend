60d82820a5bdf0797745308509219afa6c368fda
Prabha8701 <Prabha8701@gmail.com>
2023-08-17 14:31:39 +0100

    tech

---
+++ src/main/java/com/virgingames/constants/EndPoints.java
@@ -0,0 +1,15 @@
+package com.virgingames.constants;
+
+/**
+ * Created by Jay
+ */
+public class EndPoints {
+
+    /**
+     * This is Endpoints of student api
+     */
+    public static final String currencyGBP = "/bingo?currency=GBP";
+    public static final String currencyEUR = "/bingo?currency=EUR";
+    public static final String currencySEK = "/bingo?currency=SEK";
+
+}
